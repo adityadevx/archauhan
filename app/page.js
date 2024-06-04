e8f59f1443ee4af3c7ca683d5913aa03499b0eed
adityadevx <singhaditya2580@gmail.com>
2024-06-05 00:48:49 +0530

    refactor: Update page metadata for SEO optimization

--- app/page.js
+++ app/page.js
@@ -6,15 +6,17 @@ import Navbar from "./components/Navbar";
 import Timeline from "./components/Timeline";
 import Projects from "./components/Projects";
 import Header from "./components/Header";
-import Head from "next/head";
+
+
+export const metadata = {
+  title: "AR CHAUHAN",
+  description: "AR Chauhan's Portfolio",
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>AR CHAUHAN</title>
-        <meta name="description" content="Portfolio of AR Chauhan" />
-      </Head>
+     
       <Header />
       <Navbar />
       <Hero />
