60c5b6babe7db31a8b6ffb19094a45a0c6c3e2a1
adityadevx <singhaditya2580@gmail.com>
2024-06-05 00:32:01 +0530

    chore: Add Head component for SEO optimization

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Timeline from "./components/Timeline";
 import Projects from "./components/Projects";
 import Header from "./components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
