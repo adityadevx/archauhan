52354e09bafb2228e6a94d9c09f8480fb118e4c4
adityadevx <singhaditya2580@gmail.com>
2024-06-08 01:50:32 +0530

    chore: Add @vercel/analytics for page analytics tracking

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import Timeline from "../components/Timeline";
 import Projects from "../components/Projects";
 import Header from "../components/Header";
-
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "AR CHAUHAN",
@@ -16,17 +16,17 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-     
       {/* <Header /> */}
       <Navbar />
       <Hero />
-      <main className="flex min-h-screen flex-col items-center justify-between ">
+      <main className="flex min-h-screen flex-col items-center justify-between">
         <Projects />
         {/* <Tech /> */}
         {/* <Timeline /> */}
       </main>
 
       <Footer />
+      <Analytics />
     </>
   );
 }
